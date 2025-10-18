39ee138521c99ee6182355d457ec2abe7b714cbf
K. <you@example.com>
2025-10-18 11:28:48 +0530

    made changes in waybar

--- .config/waybar/scripts/hyprsunset-status.sh
+++ .config/waybar/scripts/hyprsunset-status.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if pgrep -x "hyprsunset" > /dev/null; then
-    # If hyprsunset is running, show an "on" icon
-    ICON="󰌵" # Icon for "on" or "active" (e.g., theme-light-dark)
+    # If hyprsunset is running (filter ON), show moon
+    ICON="󰖚" 
     TOOLTIP="Hyprsunset is active. Click to disable."
     CLASS="active"
 else
-    # If hyprsunset is not running, show an "off" icon
-    ICON="󰌶" # Icon for "off" or "inactive"
+    # If hyprsunset is not running (filter OFF), show sun
+    ICON="󰖜"
     TOOLTIP="Hyprsunset is inactive. Click to enable."
     CLASS="inactive"
 fi
